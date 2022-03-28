aa897d36f23f0070cbb3ad2dd75271acafd6a6bf
gytis <Gytis.kymantas@yahoo.com>
2022-03-28 17:44:21 +0300

    added subscription box ternary operator

--- kilo-health/src/components/atoms/SubscriptionBox/SubscriptionBox.tsx
+++ kilo-health/src/components/atoms/SubscriptionBox/SubscriptionBox.tsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React,{useState} from "react";
 import "./subscription-box.scss";
+import TickBox from "../../../assets/tickbox.svg";
+import TickBoxGray from "../../../assets/tickboxgray.svg";
+
 
 const SubscriptionBox = () => {
+
+    const [selected,setSelected] = useState(false);
+
+
   return (
-      <div className="subscription-box">
+      <div onClick={()=>setSelected(!selected)} className="subscription-box" style={selected ? {border:"2px solid #ff9b4e" }: {border:"2px solid rgba(0, 0, 0, 0.08)" } }>
             <div className="subscription-box--header">
-                <p>6 month plan</p>
+                <p className="monthly-plan">6 month plan</p>
                 <div className="discount-wrapper">
                     <span className="discount-price">Save 50%</span>
                 </div>
             </div>
             <div className="subscription-box--main">
-                <p><span className="large-price">&#36;9.99</span>/month</p>
-                <img src="#" alt="tickbox" />
+                <div>
+                <span className="large-price">&#x24;9.99</span>/month
+                </div>
+                <div className="tickbox">
+                    {selected? <img src={TickBox} alt="tickbox" /> : <img src={TickBoxGray} alt="tickbox" /> }
+                </div>
             </div>
             <div className="subscription-box--footer">
                <p><s className="crossed-price">&#36;119.94</s><span className="highlighted-price">&#36;59.94</span> billed every 6 months</p>
