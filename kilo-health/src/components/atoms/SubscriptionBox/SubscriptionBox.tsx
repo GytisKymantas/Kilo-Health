ece02f72460759e201fe871d5b9b33d816ebc161
gytis <Gytis.kymantas@yahoo.com>
2022-03-28 16:36:12 +0300

    added subscriptions box component

--- kilo-health/src/components/atoms/SubscriptionBox/SubscriptionBox.tsx
+++ kilo-health/src/components/atoms/SubscriptionBox/SubscriptionBox.tsx
@@ -1,8 +1,23 @@
 import React from "react";
+import "./subscription-box.scss";
 
 const SubscriptionBox = () => {
   return (
-    <div>SubscriptionBox</div>
+      <div className="subscription-box">
+            <div className="subscription-box--header">
+                <p>6 month plan</p>
+                <div className="discount-wrapper">
+                    <span className="discount-price">Save 50%</span>
+                </div>
+            </div>
+            <div className="subscription-box--main">
+                <p><span className="large-price">&#36;9.99</span>/month</p>
+                <img src="#" alt="tickbox" />
+            </div>
+            <div className="subscription-box--footer">
+               <p><s className="crossed-price">&#36;119.94</s><span className="highlighted-price">&#36;59.94</span> billed every 6 months</p>
+            </div>
+        </div>
   );
 };
 
--- kilo-health/src/page/CheckoutPage.jsx
+++ kilo-health/src/page/CheckoutPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeaderContainer from "../components/molecules/HeaderContainer/HeaderContainer";
+import SubscriptionBox from "../components/atoms/SubscriptionBox/SubscriptionBox";
 
 const CheckoutPage = () => {
   return (
     <div>
       <HeaderContainer />
+      <SubscriptionBox />
     </div>
   );
 };
